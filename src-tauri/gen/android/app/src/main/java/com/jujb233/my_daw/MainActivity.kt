b69e750cc87971f491d1f8bafa3e2c040f521a0a
jujb233 <jujiangbo@whc.edu.cn>
2025-12-01 18:49:46 +0800

    初始化

---
+++ src-tauri/gen/android/app/src/main/java/com/jujb233/my_daw/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.jujb233.my_daw
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
